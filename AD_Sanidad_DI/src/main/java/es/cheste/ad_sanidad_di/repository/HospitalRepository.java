7fb841026ed95ee726292c462f05d92346e6d89d
sergiEscriva <serescsis@alu.edu.gva.es>
2025-02-10 18:04:52 +0100

    Interfaces repository hechas

---
+++ AD_Sanidad_DI/src/main/java/es/cheste/ad_sanidad_di/repository/HospitalRepository.java
@@ -0,0 +1,30 @@
+package es.cheste.ad_sanidad_di.repository;
+
+import es.cheste.ad_sanidad_di.model.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+	/*
+	save(S entity): Guarda una entidad.
+	
+	saveAll(Iterable<S> entities): Guarda todas las entidades dadas.
+	
+	findById(ID id): Encuentra una entidad por su ID.
+	
+	existsById(ID id): Verifica si una entidad existe por su ID.
+	
+	findAll(): Encuentra todas las entidades.
+	
+	findAllById(Iterable<ID> ids): Encuentra todas las entidades por sus IDs.
+	
+	count(): Cuenta el número de entidades.
+	
+	deleteById(ID id): Elimina una entidad por su ID.
+	
+	delete(T entity): Elimina una entidad.
+	
+	deleteAll(Iterable<? extends T> entities): Elimina todas las entidades dadas.
+	
+	deleteAll(): Elimina todas las entidades
+	 */
+}
---
+++ AD_Sanidad_DI/src/main/java/es/cheste/ad_sanidad_di/repository/VisitaRepository.java
@@ -0,0 +1,29 @@
+package es.cheste.ad_sanidad_di.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitaRepository extends JpaRepository<VisitaRepository, Long> {
+	/*
+	save(S entity): Guarda una entidad.
+	
+	saveAll(Iterable<S> entities): Guarda todas las entidades dadas.
+	
+	findById(ID id): Encuentra una entidad por su ID.
+	
+	existsById(ID id): Verifica si una entidad existe por su ID.
+	
+	findAll(): Encuentra todas las entidades.
+	
+	findAllById(Iterable<ID> ids): Encuentra todas las entidades por sus IDs.
+	
+	count(): Cuenta el número de entidades.
+	
+	deleteById(ID id): Elimina una entidad por su ID.
+	
+	delete(T entity): Elimina una entidad.
+	
+	deleteAll(Iterable<? extends T> entities): Elimina todas las entidades dadas.
+	
+	deleteAll(): Elimina todas las entidades
+	 */
+}
