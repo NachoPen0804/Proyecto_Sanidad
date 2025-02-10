d384c982885785e6f2159c17221641a62d3409b2
sergiEscriva <serescsis@alu.edu.gva.es>
2025-02-10 14:16:29 +0100

    wq

---
+++ AD_Sanidad_DI/src/main/java/es/cheste/ad_sanidad_di/AdSanidadDiApplication.java
@@ -0,0 +1,13 @@
+package es.cheste.ad_sanidad_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSanidadDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdSanidadDiApplication.class, args);
+	}
+
+}
---
+++ AD_Sanidad_DI/src/test/java/es/cheste/ad_sanidad_di/AdSanidadDiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cheste.ad_sanidad_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSanidadDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
