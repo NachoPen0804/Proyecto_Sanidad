93889d1cdf86d7d74bf1e3a84e6f87f4041c5a45
sergiEscriva <serescsis@alu.edu.gva.es>
2025-02-23 19:57:55 +0100

    Falta modificar datos medico

--- AD_Sanidad_DI/src/main/java/es/cheste/ad_sanidad_di/AdSanidadDiApplication.java
+++ AD_Sanidad_DI/src/main/java/es/cheste/ad_sanidad_di/AdSanidadDiApplication.java
@@ -1,15 +1,13 @@
 package es.cheste.ad_sanidad_di;
 
 import es.cheste.ad_sanidad_di.javaFX.JavaFxApp;
-import es.cheste.ad_sanidad_di.model.*;
-import es.cheste.ad_sanidad_di.repository.*;
+import es.cheste.ad_sanidad_di.model.Usuario;
+import es.cheste.ad_sanidad_di.repository.UsuarioRepository;
 import javafx.application.Application;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class AdSanidadDiApplication {
@@ -21,22 +19,10 @@ public class AdSanidadDiApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner CargarUsuariosMedicosHospitales(UsuarioRepository repositoryUsers, MedicoRepository repositoryMedic, HospitalRepository repositoryHospitals, PacienteRepository repositoryPacients, VisitaRepository repositoryVisitas) {
+	public CommandLineRunner CargarUsuarios(UsuarioRepository repository) {
 		return (args) -> {
-			repositoryUsers.save(new Usuario("admin", "1234"));
-			repositoryUsers.save(new Usuario("user", "user123"));
-
-			Hospital hospital = new Hospital("La Prueba", "Cheste");
-			repositoryHospitals.save(hospital);
-
-			Medico medico = new Medico("Pablo", "Prueba Prueba", hospital, "prueba");
-			repositoryMedic.save(medico);
-
-			Paciente paciente = new Paciente("Pedro", "Prueba Prueba", "Cheste", "prueba");
-			repositoryPacients.save(paciente);
-
-			repositoryVisitas.save(new Visita(paciente, medico, LocalDate.of(2025,2,22)));
-
+			repository.save(new Usuario("admin", "1234"));
+			repository.save(new Usuario("user", "user123"));
 		};
 	}
 }
